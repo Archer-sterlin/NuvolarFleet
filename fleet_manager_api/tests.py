1215fea0710431d346e722d1501d110cfb643528
Rilly_ <chile.simon@4e4email.net>
2022-07-05 16:56:23 +0100

    formated and refactored code to PEP8 standard

--- fleet_manager_api/tests.py
+++ fleet_manager_api/tests.py
@@ -1,29 +1,24 @@
-from django.test import TestCase
 from django.urls import reverse
-from .models import Aircraft, AirPortInfo, Flight
 from rest_framework import status
-from rest_framework.test import APIClient, APITestCase
+from rest_framework.test import APITestCase
 
 
 class TestListCreatAircraft(APITestCase):
     def setUp(self):
         self.list_create_aircrafts_url = "list-create-aircrafts"
         self.aircraft_info = {
-                "serial_number":"yuoalr-rlaovun-niaps-anolecrab",
-                "manufacturer":"Scorpion"
-                }
+            "serial_number": "yuoalr-rlaovun-niaps-anolecrab",
+            "manufacturer": "Scorpion",
+        }
 
     def test_create_list_airport(self):
         res_get = self.client.get(reverse(self.list_create_aircrafts_url))
         res_create = self.client.post(reverse(self.list_create_aircrafts_url), self.aircraft_info)
         res_fail = self.client.post(
-                reverse(self.list_create_aircrafts_url), json={
-                "serial_number":"yuoalr-rlaovun-niaps-anolecrab",
-                "manufacturer":"Scorpion"}
+            reverse(self.list_create_aircrafts_url),
+            json={"serial_number": "yuoalr-rlaovun-niaps-anolecrab", "manufacturer": "Scorpion"},
         )
-        
+
         self.assertEqual(res_get.status_code, status.HTTP_200_OK)
         self.assertEqual(res_create.status_code, status.HTTP_201_CREATED)
         self.assertEqual(res_fail.status_code, status.HTTP_400_BAD_REQUEST)
-
-
